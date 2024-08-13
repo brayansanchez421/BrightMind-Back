79e5a5953b8b7b8a32043cea999cd1aba7d14643
Brayansanchez10 <sanchezcortesbrayan2003@gmail.com>
2024-08-13 10:16:24 -0500

    Config arreglos 2.0

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,8 @@ import users from "./user/index.js";
 import courses from './courses/index.js'
 import { authRequired } from "../middlewares/auth.middleware.js";
 
-routes.use("https://brightmind-back.onrender.com", auth);
-routes.use("https://brightmind-back.onrender.com" , users);
-routes.use("https://brightmind-back.onrender.com" , courses);
+routes.use("/", auth);
+routes.use("/" , users);
+routes.use("/" , courses);
 
 export default routes;
